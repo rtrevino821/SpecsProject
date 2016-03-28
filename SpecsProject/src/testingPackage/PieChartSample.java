46af422fa705868935573263ae798b1b5e5e7cc3
mpetitfrere <rentenkz@gmail.com>
2016-03-28 01:32:20 -0400

    updated Piechartsmaple

--- SpecsProject/src/testingPackage/PieChartSample.java
+++ SpecsProject/src/testingPackage/PieChartSample.java
@@ -27,7 +27,7 @@ public class PieChartSample extends Application {
 
 	@Override
 	public void start(Stage stage) {
-		
+		//
 		Pane root = new Pane();
 		Scene scene = new Scene(root);
 		stage.setTitle("Category Totals");
