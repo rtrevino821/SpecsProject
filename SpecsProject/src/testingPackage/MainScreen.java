1de8efe3e06fb2bd6fed64667de5a914d13acd19
JMDeS <jmichaeldeseno@gmail.com>
2016-04-21 15:53:02 -0400

    Created Infographics class

--- SpecsProject/src/testingPackage/MainScreen.java
+++ SpecsProject/src/testingPackage/MainScreen.java
@@ -2,35 +2,20 @@
 
 package testingPackage;
 
-import appPackage.sqliteConnection;
-import javafx.application.Platform;
-import javafx.embed.swing.JFXPanel;
-import javafx.scene.Scene;
-import javafx.scene.chart.*;
-
 import javax.swing.*;
 import java.awt.*;
 import java.awt.event.MouseAdapter;
 import java.awt.event.MouseEvent;
 import java.net.URISyntaxException;
-import java.sql.Connection;
-import java.util.Map;
-import java.util.TreeMap;
 
 
 public class MainScreen extends JApplet{
 
 //    private static JFrame frame;
 
-    Connection connection = null;
-
     private JFrame frame;
     private InsertPanel insertPane;
-    private JFXPanel fxPanel;
-    private JFrame fxFrame;
-    private JFXPanel chartFxPanel;
-    private JFXPanel pieChartFxPanel;
-    private Chart chart;
+    private Infographics infoGraphPane;
 
     private JLabel Logo_MainScreen;
     private JLabel lblATeamSoftware;
@@ -43,16 +28,10 @@ public class MainScreen extends JApplet{
 
     private static final int MAINSCREEN_WIDTH = 1440;
     private static final int MAINSCREEN_HEIGHT = 865;
-    private static final int FX_PANEL_WIDTH = 1000;
-    private static final int FX_PANEL_HEIGHT = 700;
-    private static final int FX_CHART_HEIGHT = 800;
-
 
     /**
     * Create the Main Application.
-    *
     */
-
     public static void main(String[] args) {
         SwingUtilities.invokeLater(() -> {
             try {
@@ -66,70 +45,21 @@ public class MainScreen extends JApplet{
 
     public MainScreen() throws URISyntaxException {
         super();
-        connection = sqliteConnection.dbConnector();
-        init();
-//        UpdateTable();
-    }
-
-    /**
-    * Initialize the contents of the frame.
-    */
-    @Override
-    public void init() {
-
-        setFXPanels();
         setFrame();
-
         setMiscLabels();
-
         setReportsIcon();
         setInsertIcon();
         setInfoGraphIcon();
         setExcelFuncIcon();
         setInterWebIcon();
         setHelpIcon();
-    }
-        
-        
-        
-        
-    private void setFXPanels() {
-        // create javafx panel for charts
-        //-----------------------------------------------------------------------------------//
-        chartFxPanel = new JFXPanel();
-        chartFxPanel.setPreferredSize(new Dimension(FX_PANEL_WIDTH, FX_PANEL_HEIGHT));
-        //-----------------------------------------------------------------------------------//
-        pieChartFxPanel = new JFXPanel();
-        pieChartFxPanel.setPreferredSize(new Dimension(FX_PANEL_WIDTH, FX_PANEL_HEIGHT));
-        //-----------------------------------------------------------------------------------//
-        fxPanel = new JFXPanel();
-        fxPanel.setBounds(0, 0, 0, 0);
-        fxPanel.setPreferredSize(new Dimension(300,300));
-    }
-
-    public void setFXFrame() {
-        fxFrame = new JFrame();
-//        fxFrame.setExtendedState(JFrame.MAXIMIZED_BOTH);
-        fxFrame.setVisible(true);
-        fxFrame.getContentPane().setBackground(new Color(244, 244, 244));
-        fxFrame.setFont(new Font("Segoe UI Semilight", Font.PLAIN, 12));
-        fxFrame.setTitle("Insert Asset");
-        fxFrame.setBounds(100, 100, 1504, 793);
-        fxFrame.setDefaultCloseOperation(JFrame.DISPOSE_ON_CLOSE);
-        fxFrame.setLocationRelativeTo(null);
-        fxFrame.getContentPane().setLayout(null);
-        fxFrame.getContentPane().setLayout(new SpringLayout());
-        fxFrame.setSize(new Dimension(FX_PANEL_WIDTH, FX_CHART_HEIGHT));
-
-        ImageIcon icon = new ImageIcon(getClass().getResource("/Resources/appIconImage.png"));
-        fxFrame.setIconImage(icon.getImage());
+//        UpdateTable();
     }
 
     private void setFrame() {
         frame = new JFrame();
         frame.setIconImage(Toolkit.getDefaultToolkit().getImage(MainScreen.class.getResource("/Resources/appIconImage.png")));
         frame.getContentPane().setLayout(null);
-        frame.getContentPane().add(fxPanel);
         frame.setVisible(true);
         frame.getContentPane().setBackground(new Color(244, 244, 244));
         frame.setBackground(Color.WHITE);
@@ -137,35 +67,6 @@ public class MainScreen extends JApplet{
         frame.setSize(MAINSCREEN_WIDTH, MAINSCREEN_HEIGHT);
         frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
         frame.setLocationRelativeTo(null);
-    }
-
-    private void createScene() {
-        chart = createBarChart();
-        chartFxPanel.setScene(new Scene(chart));
-    }
-
-    private void createPieScene() {
-        PieChartSample pcs = new PieChartSample();
-        chart = new PieChart(pcs.getPieChartData());
-        pieChartFxPanel.setScene(new Scene(chart));
-    }
-
-    private BarChart createBarChart() {
-        XYChart.Series series = new XYChart.Series();
-        TreeMap<String,Double> map = testingPackage.LineChartSample.test_Everything_Total_Spent();
-        for(Map.Entry<String,Double> e : map.entrySet()){
-            series.getData().add(new XYChart.Data(e.getKey(), e.getValue()));
-        }
-        CategoryAxis xAxis = new CategoryAxis();
-        xAxis.setLabel("Year");
-
-        NumberAxis yAxis = new NumberAxis();
-        yAxis.setLabel("Units Sold");
-
-        final BarChart chart = new BarChart(xAxis, yAxis);
-        chart.getData().add(series);
-
-        return chart;
     }
 
     private void setMiscLabels() {
@@ -181,6 +82,7 @@ public class MainScreen extends JApplet{
         lblATeamSoftware.setFont(new Font("Segoe UI Semilight", Font.PLAIN, 20));
         frame.getContentPane().add(lblATeamSoftware);
     }
+
     //------BUTTON SETUP METHODS----------//
 
     private void setReportsIcon() {
@@ -254,22 +156,9 @@ public class MainScreen extends JApplet{
             }
             @Override
             public void mouseClicked(MouseEvent arg0) {
-                try {
-                    // create JavaFX scene
-                    setFXFrame();
-                    JTabbedPane tabbedPane = new JTabbedPane();
-                    fxFrame.getContentPane().add(tabbedPane);
-
-                    Platform.runLater(() -> createScene());
-                    tabbedPane.addTab("Bar Chart", chartFxPanel);
-
-                    Platform.runLater(() -> createPieScene());
-                    tabbedPane.addTab("Pie Chart", pieChartFxPanel);
-
-                    tabbedPane.setVisible(true);
-
-                    //report.setVisible(true);
-                } catch (Exception e) {e.printStackTrace();}
+//                try {
+                    infoGraphPane = new Infographics();
+//                } catch (Exception e) {e.printStackTrace();}
             }
         });
     }
